1781796e8c03f908a941e7e1938474a6e86189b4
TehIvan <83208329+TehIvan@users.noreply.github.com>
2023-01-03 23:11:04 +0100

    Init

---
+++ src/main/java/com/devnics/olivia/Olivia.java
@@ -0,0 +1,14 @@
+package com.devnics.olivia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Olivia extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hello World -> Test");
+    }
+
+    @Override
+    public void onDisable() {}
+}
